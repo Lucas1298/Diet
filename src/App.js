6f76d6e059a3900acacc1c4a601de550a80139fa
Lucas1298 <lcsrch98@gmail.com>
2021-05-05 06:52:40 -0300

    -add onAuthStateChanged to dispactch email in redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
-import './App.scss';
+import React, { Component } from 'react';
+
+import { connect } from "react-redux"
+
+import { Switch, Route } from 'react-router-dom';
+
+import { setCurrentUser } from "./redux/user/user.action"
 
 import Principal from "./pages/principal/Principal"
+import Home from "./pages/home/home"
 
-function App() {
-  return (
-    <div classname="app" >
-      <Principal/>
-    </div>
-  );
+import { auth } from "./firebase/firebase.util"
+
+import './App.scss';
+
+class App extends Component {
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    const {dispatchSetCurrentUser } = this.props
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        dispatchSetCurrentUser(userAuth.email)
+      
+      }else{
+        dispatchSetCurrentUser(userAuth)
+      }
+    })
+
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    const { currentUser } = this.props
+    return (
+      <div className="app" >
+        <Switch>
+          <Route exact path="/" render={()=> currentUser ? <Home/> : <Principal/>}/>
+        </Switch>
+        
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = ({user}) =>({
+  currentUser: user.currentUser
+})
+
+
+const mapDispatchToProps = dispatch =>({
+  dispatchSetCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
